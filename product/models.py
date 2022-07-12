3019d667ff6826f2b6195013ba6eacc4584ea865
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-12 09:05:23 +0545

    1 commit

--- product/models.py
+++ product/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    name = models.TextField()
+    price = models.TextField()
+    summary = models.TextField()
